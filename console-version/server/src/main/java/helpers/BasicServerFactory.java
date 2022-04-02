e020a82a5daafc0510e85081e587bb5919192bc3
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-04-02 23:33:26 +0200

    [CHAT-3] ChatServer basic configuration

---
+++ console-version/server/src/main/java/helpers/BasicServerFactory.java
@@ -0,0 +1,26 @@
+package helpers;
+
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BasicServerFactory implements ServerFactory {
+
+    @Override
+    public Logger createLogger(Class<?> clazz) {
+        return Logger.getLogger(clazz);
+    }
+
+    @Override
+    public ExecutorService createExecutorService(ExecutorServiceType type, int poolSize) {
+        switch (type) {
+            case FIXED:
+                return Executors.newFixedThreadPool(poolSize);
+            case SCHEDULED:
+                return Executors.newScheduledThreadPool(poolSize);
+            default:
+                return Executors.newSingleThreadExecutor();
+        }
+    }
+}
