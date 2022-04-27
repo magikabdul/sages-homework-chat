abdd7b5c00d1f0528ecffb12fb6ab4decd505404
Krzysztof Cholewa - Ubuntu <krzysztof.cholewa@gmail.com>
2022-04-27 19:20:28 +0200

    [CHAR-REST-3] flyway configuration

---
+++ rest-version/src/main/java/cloud/cholewa/chat/infrastructure/config/FlywayMigrationExecutor.java
@@ -0,0 +1,30 @@
+package cloud.cholewa.chat.infrastructure.config;
+
+import org.flywaydb.core.Flyway;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.sql.DataSource;
+
+@Startup
+@Singleton
+@TransactionManagement(TransactionManagementType.BEAN)
+public class FlywayMigrationExecutor {
+
+    @Resource(lookup = "java:jboss/datasources/PostgresDS")
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void migrate() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .schemas("public")
+                .load();
+//        flyway.baseline();
+        flyway.migrate();
+    }
+}
---
+++ rest-version/src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    nick VARCHAR(50),
+    password VARCHAR(50),
+    token VARCHAR(50)
+);
