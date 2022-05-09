05742979eda1da4ffb8d1877eab69c2011f3141b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-05-09 23:16:20 +0200

    [REST-CHAT-13] fixed issue with closing app from welcome menu

--- rest-client/src/main/java/cloud/cholewa/rest_client/domain/ClientService.java
+++ rest-client/src/main/java/cloud/cholewa/rest_client/domain/ClientService.java
@@ -22,6 +22,7 @@ public class ClientService implements ClientServicePort {
 
     public static final String END_SESSION = "\\q";
     public static final String GET_HISTORY = "\\h";
+    public static final String CHANNEL = "\\c";
 
     private String userName;
     private String token;
@@ -46,7 +47,7 @@ public class ClientService implements ClientServicePort {
                     executeRegister();
                     done = executeLogin();
                 }
-                case "3" -> done = true;
+                case "3" -> System.exit(0);
                 default -> Console.errorMessage("Invalid option", true);
             }
         }
@@ -66,10 +67,16 @@ public class ClientService implements ClientServicePort {
             switch (command) {
                 case END_SESSION -> done = true;
                 case GET_HISTORY -> showHistory();
+                case CHANNEL -> handleChannel();
                 default -> publishMessage(command);
             }
         }
     }
+
+    private void handleChannel() {
+
+    }
+
 
     private void publishMessage(String message) {
         var response = apiGateway.sendMessage(
@@ -89,13 +96,16 @@ public class ClientService implements ClientServicePort {
         long lastMessageId = 0;
 
         while (true) {
-            var response = apiGateway.getLastChannelMessage(token)
-                    .readEntity(MessageHistoryDto.class);
+            var response = apiGateway.getLastChannelMessage(token);
 
-            if (lastMessageId != response.getId() && myLastMessageId != response.getId()) {
-                lastMessageId = response.getId();
-                Console.chatMessage(response);
-                Console.advancedPrompt(channel, userName);
+            if (response.getStatus() != HttpStatus.SC_NOT_FOUND) {
+                MessageHistoryDto messageHistoryDto = response.readEntity(MessageHistoryDto.class);
+
+                if (lastMessageId != messageHistoryDto.getId() && myLastMessageId != messageHistoryDto.getId()) {
+                    lastMessageId = messageHistoryDto.getId();
+                    Console.chatMessage(messageHistoryDto);
+                    Console.advancedPrompt(channel, userName);
+                }
             }
 
             TimeUnit.SECONDS.sleep(1);
